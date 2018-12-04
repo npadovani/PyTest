6a5aabc18fa5cf6ded3a755bc0bf95db59d238bf
Nicola Padovani <n.t.g.padovani@gmail.com>
2018-12-04 09:34:08 +0000

    First commit original code

---
+++ Exercise Files/Ch2/Test Intentions in Pycharm.py
@@ -0,0 +1,29 @@
+
+import datetime as dt
+
+dt.datetime.now()
+
+import random as rd
+
+rd.choice(["blue","green","yellow"])
+rd.randint(-1,150)
+rd.seed(1234)
+rd.randint(0,1)
+
+import numpy as np
+
+theta = np.random.randint(10,100,1000)
+
+print(theta)
+
+def sticazzi(arg1,arg2):
+    """ this is a test help for function sticazzi \n
+    it takes two variables \n
+    arg1 amd arg2"""
+    print(arg1+arg2)
+
+argomento1 ="e allora"
+argomento2  = "mortacci"
+sticazzi(arg1=argomento1,arg2=argomento2)
+
+sticazzi(argomento2,argomento1)
